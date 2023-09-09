6a111f2d3b030d6b80ddefb8aab281f691339fe5
alwubin <135022491+yoobinseo24@users.noreply.github.com>
2023-09-09 12:45:50 +0800

    [Bronze V] Title: A+B - 7, Time: 120 ms, Memory: 9360 KB -BaekjoonHub

---
+++ 백준/Bronze/11021. A＋B － 7/A＋B － 7.js
@@ -0,0 +1,12 @@
+let fs = require('fs');
+let input = fs.readFileSync('/dev/stdin').toString().split('\n');
+
+let n = Number(input[0]);
+let result = '';
+
+for (let i = 1; i <= n; i++) {
+    let x = Number(input[i].split(' ')[0]);
+    let y = Number(input[i].split(' ')[1]);
+    result += 'Case #'+i+': '+(x+y)+'\n';
+}
+console.log(result);
