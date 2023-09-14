da5c75bab3fe3eeae766ba8025ec5ae091f44cb0
Yoobin Seo <135022491+yoobinseo24@users.noreply.github.com>
2023-09-14 09:41:23 +0800

    [Silver V] Title: 수 정렬하기 5, Time: 19108 ms, Memory: 220836 KB -BaekjoonHub

---
+++ 백준/Silver/15688. 수 정렬하기 5/수 정렬하기 5.js
@@ -0,0 +1,15 @@
+let fs = require('fs');
+let input = fs.readFileSync('/dev/stdin').toString().split('\n');
+
+let n = Number(input[0]);
+let arr = [];
+
+for (let i = 1; i <= n; i++) {
+  arr.push(Number(input[i]));
+}
+
+arr.sort(function(a,b) {
+  return a-b;
+})
+
+console.log(arr.join('\n'));
