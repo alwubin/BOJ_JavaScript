a1527ab75b7f632d0f57e9f89d8d5aee232f46ba
Yoobin Seo <135022491+alwubin@users.noreply.github.com>
2023-09-26 23:15:19 +0800

    [Silver III] Title: 모든 순열, Time: 376 ms, Memory: 40528 KB -BaekjoonHub

---
+++ 백준/Silver/10974. 모든 순열/모든 순열.js
@@ -0,0 +1,30 @@
+let fs = require('fs');
+let input = fs.readFileSync('/dev/stdin').toString().split('\n');
+
+let n = Number(input[0]);
+let arr = [];
+for (let i = 1; i <= n; i++) arr.push(i);
+let visited = new Array(n).fill(false);
+let selected = [];
+
+let ans = '';
+function dfs(arr, depth) {
+    if (depth == n) {
+        let result = [];
+        for (let i of selected) result.push(arr[i]);
+        for (let x of result) ans += x + ' ';
+        ans += '\n';
+        return;
+    }
+    for (let i= 0; i <n; i++) {
+        if (visited[i]) continue;
+        selected.push(i);
+        visited[i] = true;
+        dfs(arr, depth+1);
+        selected.pop();
+        visited[i] = false;
+    }
+}
+
+dfs(arr,0);
+console.log(ans.slice(0,-1));
