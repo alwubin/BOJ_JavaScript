191fb155d0f84b58b7bc214d200ce86cebfbde42
Yoobin Seo <135022491+alwubin@users.noreply.github.com>
2023-10-18 15:08:32 +0800

    [level 1] Title: 과일 장수, Time: 0.09 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/1/135808. 과일 장수/과일 장수.js
@@ -0,0 +1,9 @@
+function solution(k, m, score) {
+    score.sort((a,b) => b-a);
+    let result = 0;
+    
+    for (let i = m-1; i < score.length; i+=m) {
+        result += score[i]*m;
+    }
+    return result;
+}
