4b148bc11272f561e1c16c82dca7a0e370dd1e9c
Yoobin Seo <135022491+alwubin@users.noreply.github.com>
2023-11-10 08:39:21 +0800

    [level 1] Title: 가운데 글자 가져오기, Time: 0.04 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/1/12903. 가운데 글자 가져오기/가운데 글자 가져오기.js
@@ -0,0 +1,9 @@
+function solution(s) {
+    let answer;
+    if (s.length % 2 !== 0) {
+        answer = s.substring(parseInt(s.length/2),parseInt(s.length/2)+1);
+    }
+    else answer = s.substring(parseInt(s.length/2)-1,parseInt(s.length/2)+1);
+
+    return answer;
+}
