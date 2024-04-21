be4b200603da76080c74fa4356a12e01ec472d0a
Yoobin Seo <135022491+alwubin@users.noreply.github.com>
2024-04-21 16:13:20 +0900

    [Gold IV] Title: 알파벳, Time: 2620 ms, Memory: 12964 KB -BaekjoonHub

--- 백준/Gold/1987. 알파벳/알파벳.js
+++ 백준/Gold/1987. 알파벳/알파벳.js
@@ -1,35 +1,35 @@
-const [n,...input] = require("fs").readFileSync("/dev/stdin").toString().trim().split("\n");
+let fs = require('fs');
+let input = fs.readFileSync('/dev/stdin').toString().split('\n');
 
-const [maxY, maxX] = n.split(" ").map(Number);
-const graph = input.map((value) => value.split(""));
+const [r, c] = input.shift().split(' ').map(Number);
+const graph = input.map((value) => value.split(''));
+const alphabet = new Array(26).fill(false);
 
-const dx = [0, 0, 1, -1];
-const dy = [1, -1, 0, 0];
-
+const dir = [
+  [-1, 0],
+  [0, 1],
+  [1, 0],
+  [0, -1],
+];
 let count = 0;
 
-const visited = new Set(graph[0][0]);
-
-
-function dfs(depth, y, x) {
-    count = Math.max(count, depth);
-    for(let i = 0; i<4; i++) {
-        if(check(y+dy[i], x+dx[i]) == false) continue;
-        visited.add(graph[y+dy[i]][x+dx[i]]);
-        dfs(depth + 1, y+dy[i], x+dx[i]);
-        visited.delete(graph[y+dy[i]][x+dx[i]]);
-    }
-
-}
-
-function check(y, x) {
-    if(y < 0 || x < 0 || x>= maxX || y >= maxY) {
-        return false;
-    } else if(visited.has(graph[y][x])) {
-        return false;
-    }
-    return true;
-}
-
-dfs(1,0,0);
+dfs(0, 0, 1);
 console.log(count);
+
+
+function dfs(i, j, cellCnt) {
+  alphabet[graph[i][j].charCodeAt() - 65] = true;
+  count = Math.max(count, cellCnt);
+  for(let d of dir) {
+    const [dx, dy] = [i + d[0], j + d[1]];
+    if(isValidRange(dx,dy) && !alphabet[graph[dx][dy].charCodeAt() - 65]) {
+      dfs(dx, dy, cellCnt + 1);
+      alphabet[graph[dx][dy].charCodeAt() - 65] = false;
+    }
+  }
+}
+
+function isValidRange(i, j) {
+  if (i < 0 || i >= r || j < 0 || j >= c) return false;
+  else return true;
+}
